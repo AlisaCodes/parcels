32a1207ad3d0298d8665975e4685d65d737e245e
Austin Kincaid & Alisa Colon <student@epicodus.com>
2015-08-10 11:14:16 -0700

    Add spec and method to return cost of shipping

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -4,9 +4,31 @@ class Parcel
     @width = width
     @length = length
     @weight = weight
+    @volume = height * width * length
   end
 
   define_method(:volume) do
-    volume = @height * @width * @length
+    @volume
+  end
+
+  define_method(:cost_to_ship) do |distance, speed|
+    base_price = 5
+    running_total = @weight * 1.5
+    multiplier = 1
+
+    if @volume > 40
+      multiplier += 0.5
+    end
+
+    if speed == 2
+      multiplier += 0.5
+    end
+
+    if distance > 50
+      multiplier += 1
+    end
+
+    running_total = running_total * multiplier
+
   end
 end
--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -8,4 +8,11 @@ describe(Parcel) do
       expect(test_parcel.volume()).to(eq(24))
     end
   end
+
+  describe('#cost_to_ship') do
+    it('returns cost of shipment of a short distance regular small shipment') do
+      test_parcel = Parcel.new(4, 3, 2, 5)
+      expect(test_parcel.cost_to_ship(25, 1)).to(eq(7.5))
+    end
+  end
 end
