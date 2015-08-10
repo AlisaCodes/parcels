a0f14b3a8dcb59fde61726fcc43ba85d9e826c48
Austin Kincaid & Alisa Colon <student@epicodus.com>
2015-08-10 11:18:41 -0700

    Add spec to confirm multiplier compliance for cost_to_ship

--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -14,5 +14,11 @@ describe(Parcel) do
       test_parcel = Parcel.new(4, 3, 2, 5)
       expect(test_parcel.cost_to_ship(25, 1)).to(eq(7.5))
     end
+
+    it('returns cost of shipment of a long distance fast large shipment') do
+      test_parcel = Parcel.new(4, 7, 4, 10)
+      expect(test_parcel.cost_to_ship(51, 2)).to(eq(45))
+    end
+
   end
 end
